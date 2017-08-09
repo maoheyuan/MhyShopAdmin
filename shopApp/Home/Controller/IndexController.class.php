d22e7b5431d574f020589cc5ade1e9fc1c817404
“maoheyuan <maoheyuan@qq.com>
2017-08-09 15:23:28 +0800

    前台solr的测试

--- shopApp/Home/Controller/IndexController.class.php
+++ shopApp/Home/Controller/IndexController.class.php
@@ -1,8 +1,37 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+use Home\Helper\Solr;
 class IndexController extends Controller {
     public function index(){
-        $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+
+
+
+        header("Content-type: text/html; charset=utf-8");
+
+
+        $coreName = 'mytest';
+        Solr::setCore($coreName);
+
+   /*     //查询
+        $qwhere = array(
+            "username" => "si sheng chao",
+        );
+        print_r(Solr::selectQuery($qwhere));*/
+
+
+
+        for($i=0;$i<=10;$i++){
+
+            //添加
+            $fieldArr = array(
+                "id" => time(),
+                "username" => "chao".time(),
+                "usertype" => 1,
+                "editTime" => time(),
+            );
+          var_dump(  Solr::addDocument($fieldArr));
+        }
+
     }
 }
---
+++ shopApp/Home/Helper/Solr.class.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/8/9
+ * Time: 13:59
+ */
+
+namespace Home\Helper;
+
+
+class Solr {
+
+
+    //solr服务器地址及端口设置
+    private static $options = array('hostname' => 'localhost','port' => '8983');
+
+    /**
+     * 设置solr库选择
+     * @param $core string 库名称
+     */
+    public static function setCore($core){
+        if($core) self::$options['path']='solr/'.$core;
+    }
+
+    /**
+     * 增加solr索引
+     * @param $fieldArr 索引字段及值
+     * @return bool true
+     */
+    public static function addDocument($fieldArr=array()){
+        $client = new \SolrClient(self::$options);
+        $doc = new \SolrInputDocument();
+        foreach($fieldArr as $k => $v){
+            $doc->addField($k,$v);
+        }
+        $client->addDocument($doc);
+        return $client->commit();
+     
+    }
+
+    /**
+     * 删除索引
+     * @param $id 主键id id可以为数组形式，应用于多选的情况
+     * @return bool true
+     */
+    public static function delDocument($ids){
+        $client = new \SolrClient(self::$options);
+        if(is_array($ids))
+            $client->deleteByIds($ids);
+        else
+            $client->deleteById($ids);
+        $client->commit();
+        return true;
+    }
+
+    /**
+     * 查询数据
+     * @param $qwhere     关键字
+     * @param $fqwhere 附加条件，根据范围检索，适用于数值型
+     * @param $getField    查询字段
+     * @param $sort 排序 array('duration'=>'asc')  asc:升序，desc:降序
+     * @param $pageindex   查询页数
+     * @param $pagesize    每页显示条数
+     */
+    public static function selectQuery($qwhere=array(),$fqwhere=array(),$getField=array(),$sort=array(),$pageindex=1,$pagesize=20){
+        $client = new \SolrClient(self::$options);
+        $query = new \SolrQuery();
+        $sel = '';
+        foreach($qwhere as $k => $v){
+            $sel .= ' +'.$k.':'.$v;        //对中文分词的field用这行
+//        $sel = "{$k} : \"*{$v}*\"";    //对英文貌似$v两侧加*号就能模糊搜索了
+        }
+        $query->setQuery($sel);
+        //关键字检索
+
+        //附加条件，根据范围检索，适用于数值型
+        if($fqwhere){
+            $query->setFacet(true);
+            foreach($fqwhere as $k => $v)
+                $query->addFacetQuery($v);
+            //$query->addFacetQuery('price:[* TO 500]');
+        }
+
+        //查询字段
+        if($getField){
+            foreach($getField as $key => $val)
+                $query->addField($val);
+        }
+        //排序
+        if($sort){
+            foreach($sort as $k => $v){
+                if($v == 'asc')
+                    $query->addSortField($k,SolrQuery::ORDER_ASC);
+                elseif($v == 'desc')
+                    $query->addSortField($k,SolrQuery::ORDER_DESC);
+            }
+        }
+        //分页
+        $query->setStart(self::getPageIndex($pageindex,$pagesize));
+        $query->setRows($pagesize);
+
+        $query_response = $client->query($query);
+        $response = $query_response->getResponse();
+        return $response;
+    }
+
+    /**
+     * 分页数据处理
+     */
+    private static function getPageIndex($pageindex,$pagesize){
+        if($pageindex<=1)
+            $pageindex = 0;
+        else
+            $pageindex = ($pageindex-1)*$pagesize;
+        return $pageindex;
+    }
+
+}
